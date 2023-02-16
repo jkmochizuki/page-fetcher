44e261be030ffa8486fc3aa8bb45a5d4bde80c23
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-15 18:15:11 -0800

    refactored code

--- fetcher.js
+++ fetcher.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const downloadResource = (URL, localPath) => {
   request(URL.join(""), (error, response, body) => {
     fs.writeFile(localPath.join(""), body, err => {
-      if (err) {
-        console.error(err);
-      }
-      let fileSize = body.length;
+      if (err) console.error(err);
+      const fileSize = body.length;
       console.log(`Downloaded and saved ${fileSize} bytes to ${localPath}`);
     });
   });
